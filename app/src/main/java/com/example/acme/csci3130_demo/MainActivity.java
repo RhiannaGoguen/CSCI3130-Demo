71dd191cb5b1d702f81d6cd3aaedc5fdae4a6098
Rhianna Goguen <rhianna.goguen@dal.ca>
2018-05-17 20:39:00 -0300

    Added a button and a text field. When the button is clicked, the text inputted in the text field overwrites the hello world text in the middle of the view

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                TextView textView = findViewById(R.id.textView);
+                EditText editText = findViewById(R.id.editText);
+                Editable contents = editText.getText();
+                textView.setText(contents);
+            }
+        });
+
+
     }
+
+
 }
